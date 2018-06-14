a26392322d09e6a851cecf5a350e0d865a374ce6
Francesco Sardara <francesco.sardara@ausy.be>
2018-06-14 16:25:39 +0200

    OPENEUROPA-644: Create an europa_demo module and provide a few blocks.

---
+++ modules/europa_demo/src/Plugin/Block/FooterCorporateTopBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\europa_demo\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Footer corporate top' block.
+ *
+ * @Block(
+ *   id = "europa_demo_footer_corporate_top",
+ *   admin_label = @Translation("Footer corporate - top"),
+ *   category = @Translation("Europa Demo")
+ * )
+ */
+class FooterCorporateTopBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build = [
+      '#theme' => 'europa_demo_footer_corporate_top',
+    ];
+
+    return $build;
+  }
+
+}
---
+++ modules/europa_demo/src/Plugin/Block/SiteSwitcherBlock.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\europa_demo\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Site switcher' block.
+ *
+ * @Block(
+ *   id = "europa_demo_site_switcher",
+ *   admin_label = @Translation("EC Site switcher"),
+ *   category = @Translation("Europa Demo")
+ * )
+ */
+class SiteSwitcherBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+    $build = [
+      '#theme' => 'europa_demo_site_switcher',
+    ];
+
+    return $build;
+  }
+
+}
