b07781d0a5cae0b2163c9b2de030cae15e25f0f4
Imanol Eguskiza <ier@opendeusto.es>
2018-06-19 12:30:17 +0200

    OPENEUROPA-646: Changed base class for plugin.

--- modules/europa_demo_core/src/Plugin/PageHeaderMetadata/LandingPagePageHeaderMetadata.php
+++ modules/europa_demo_core/src/Plugin/PageHeaderMetadata/LandingPagePageHeaderMetadata.php
@@ -5,11 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\europa_demo_core\Plugin\PageHeaderMetadata;
 
 use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\Core\Routing\RouteMatchInterface;
-use Drupal\oe_theme_helper\PageHeaderMetadataPluginBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\oe_theme_helper\Plugin\PageHeaderMetadata\EntityCanonicalRoutePageHeaderMetadata;
 
 /**
  * Defines a page header metadata plugin that extracts data from current entity.
@@ -19,44 +15,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   label = @Translation("Landing page metadata extractor")
  * )
  */
-class LandingPagePageHeaderMetadata extends PageHeaderMetadataPluginBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The current route match.
-   *
-   * @var \Drupal\Core\Routing\RouteMatchInterface
-   */
-  protected $currentRouteMatch;
-
-  /**
-   * Creates a new EntityPageHeaderMetadata object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Routing\RouteMatchInterface $current_route_match
-   *   The current route match.
-   */
-  public function __construct(array $configuration, string $plugin_id, $plugin_definition, RouteMatchInterface $current_route_match) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-
-    $this->currentRouteMatch = $current_route_match;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('current_route_match')
-    );
-  }
+class LandingPagePageHeaderMetadata extends EntityCanonicalRoutePageHeaderMetadata {
 
   /**
    * {@inheritdoc}
@@ -74,7 +33,7 @@ class LandingPagePageHeaderMetadata extends PageHeaderMetadataPluginBase impleme
 
     // Get values from entity.
     $title = $entity->label();
-    $introduction = !empty($entity->get('field_introduction')->getValue())? $entity->get('field_introduction')->getString() : '';
+    $introduction = !$entity->get('field_introduction')->isEmpty() ? $entity->get('field_introduction')->getString() : '';
     $metadata = [
       'title' => $title,
       'introduction' => $introduction,
@@ -87,25 +46,6 @@ class LandingPagePageHeaderMetadata extends PageHeaderMetadataPluginBase impleme
       ->applyTo($metadata);
 
     return $metadata;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getEntityFromCurrentRoute(): ?ContentEntityInterface {
-    if (($route = $this->currentRouteMatch->getRouteObject()) && ($parameters = $route->getOption('parameters'))) {
-      // Determine if the current route represents an entity.
-      foreach ($parameters as $name => $options) {
-        if (isset($options['type']) && strpos($options['type'], 'entity:') === 0) {
-          $entity = $this->currentRouteMatch->getParameter($name);
-          if ($entity instanceof ContentEntityInterface && $this->currentRouteMatch->getRouteName() === "entity.{$entity->getEntityTypeId()}.canonical") {
-            return $entity;
-          }
-        }
-      }
-    }
-
-    return NULL;
   }
 
 }
