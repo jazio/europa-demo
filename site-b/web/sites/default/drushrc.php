69dc513eaaf539ddf9a7fab54877c3d2822103da
Antonio De Marco <antonio@nuvole.org>
2018-08-20 15:16:23 +0200

    OPENEUROPA-1109: Remove files generated by the Task Runner.

--- site-b/web/sites/default/drushrc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Start settings processor block.
-
-$options["uri"] = 'http://web:8080/site-b/web';
-
-// End settings processor block.
--- site-c/web/sites/default/drushrc.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Start settings processor block.
-
-$options["uri"] = 'http://web:8080/web';
-
-// End settings processor block.
