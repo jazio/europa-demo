1132843a67f22cebf2b3e5b21d74ddd017b2db6f
Antonio De Marco <antonio@nuvole.org>
2018-09-04 14:09:49 +0200

    OPENEUROPA-1161: Fix settings.php generation.

--- sites/agri/web/sites/default/default.settings.php
+++ sites/agri/web/sites/default/default.settings.php
@@ -768,9 +768,3 @@ $settings['entity_update_batch_size'] = 50;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-
-// Start settings processor block.
-
-$config_directories["sync"] = '../config/sync';
-
-// End settings processor block.
