ca854f36b754087c4fa68fdb9597c33e2aba3c72
Antonio De Marco <antonio@nuvole.org>
2018-09-15 09:31:47 +0200

    OPENEUROPA-1186: Use entity_list pattern context in News controller.

--- modules/europa_demo_core/src/Controller/NewsController.php
+++ modules/europa_demo_core/src/Controller/NewsController.php
@@ -17,23 +17,22 @@ class NewsController extends RdfEntityPageControllerBase {
       ->condition('rid', 'oe_announcement')
       ->execute();
 
-    $announcements = $this->entityTypeManager->getStorage('rdf_entity')->loadMultiple($ids);
-    $items = [];
-
-    foreach ($announcements as $announcement) {
-    }
-
-    $build = [
-      '#cache' => [
-        'tags' => ['rdf_entity_list'],
-      ],
-    ];
-
+    // Use list item block pattern to render announcement list.
+    // We set a custom pattern context so that we can get advantage of theme
+    // suggestions and preprocess set via hook_ui_patterns_suggestions_alter().
+    // @see europa_demo_core_ui_patterns_suggestions_alter()
+    // @see europa_demo_theme_preprocess_pattern_list_item_block_one_column__entity_list__rdf_entity__announcement()
     $build[] = [
       '#type' => 'pattern',
       '#id' => 'list_item_block_one_column',
-      '#fields' => [
-        'items' => $items,
+      '#context' => [
+        'type' => 'entity_list',
+        'entity_type' => 'rdf_entity',
+        'bundle' => 'announcement',
+        'entities' => $this->entityTypeManager->getStorage('rdf_entity')->loadMultiple($ids),
+      ],
+      '#cache' => [
+        'tags' => ['rdf_entity_list'],
       ],
     ];
 
